95c389e2ab7e4b324d53e1c8a5db2f3cb37fd967
Gianluca <gianluca.sasanelli@gmail.com>
2025-12-28 17:12:34 +0100

    chore: Changed name of database url env variable

--- frontend/drizzle.config.ts
+++ frontend/drizzle.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
+if (!process.env.POSTGRES_URL) {
+  throw new Error("POSTGRES_URL is not set");
 }
-console.log("DATABASE_URL", process.env.DATABASE_URL);
+console.log("POSTGRES_URL", process.env.POSTGRES_URL);
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.POSTGRES_URL,
   },
   migrations: {
     table: 'migrations',
