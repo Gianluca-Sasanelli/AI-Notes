452d9555f5e067a9432a10582e9d318849740d31
Gianluca <gianluca.sasanelli@gmail.com>
2025-12-28 17:12:34 +0100

    chore: Changed name of database url env variable

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,10 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 
-const DATABASE_URL = process.env.DATABASE_URL
-if (!DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set")
+const POSTGRES_URL = process.env.POSTGRES_URL
+if (!POSTGRES_URL) {
+  throw new Error("POSTGRES_URL is not set")
 }
 
-const client = postgres(DATABASE_URL)
+const client = postgres(POSTGRES_URL)
 export const db = drizzle(client)
