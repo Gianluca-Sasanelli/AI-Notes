cda074b69308f23459ea1c54c444343d3b4288e2
Gianluca <gianluca.sasanelli@gmail.com>
2026-01-06 17:39:08 +0100

    chore: Added basic file sanitaziong for S3 api and max file to 10 mb

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   serverExternalPackages: ["@aws-sdk/client-s3", "@aws-sdk/s3-request-presigner"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
